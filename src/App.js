4844460fa9464cac3c119a2e8876752054f94262
Justin <justins192107@gmail.com>
2020-12-21 02:02:02 -0330

    setting up styled-components

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Main = styled.div`
+  height: 100vh;
+  width: 100vw;
+`
+
+function App() {
+  return (
+    <Main>
+      <h1>
+        Pomodoro Timer
+      </h1>
+    </Main>
+  )
+}
+
+export default App;
