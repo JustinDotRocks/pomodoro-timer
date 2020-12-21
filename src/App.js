857978848c1b55320c341452e2247536db5fa847
Justin <justins192107@gmail.com>
2020-12-21 02:31:37 -0330

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 const Main = styled.div`
   height: 100vh;
@@ -8,12 +15,41 @@ const Main = styled.div`
 
 function App() {
   return (
-    <Main>
-      <h1>
-        Pomodoro Timer
-      </h1>
-    </Main>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/settings">Settings</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/settings">
+            <Main>
+              <h1>
+                Settings
+              </h1>
+            </Main>
+          </Route>
+          <Route path="/">
+            <Main>
+              <h1>
+                Pomodoro Timer
+              </h1>
+            </Main>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
 export default App;
+
