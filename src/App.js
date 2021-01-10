c00e3e841f0bdf81cfb60b1b90a02ff2cda52ee9
Justin <justins192107@gmail.com>
2021-01-09 23:54:07 -0330

    Extract Navbar component from App component

--- src/App.js
+++ src/App.js
@@ -3,42 +3,18 @@ import styled from 'styled-components';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 
 import CountdownTimer from './components/TimerSettings';
+import Navbar from './components/navbar/Navbar';
 
-const NavBarContainer = styled.nav`
-  background-color: #0089aa;
-  display: flex;
-  align-items: center;
-
-  height: 35px;
-
-  border-bottom: 3px solid #008894;
-
-`
-
-const NavItemsList = styled.ul`
-  display: flex;
-  flex-direction: row;
-  list-style-type: none;
-
-`
-const NavItem = styled.li`
-  margin-left: 1.2rem;
-
-  > * {
-    text-decoration: none;
-    color: white;
-  }
-`
 
 const Main = styled.div`
   height: 100vh;
   width: 100vw;
 `
+
 const CountdownTimerContainer = styled.div`
   font-size: 2.5rem;
 `
@@ -47,17 +23,7 @@ function App() {
   return (
     <Router>
       <div>
-        <NavBarContainer>
-          <NavItemsList>
-            <NavItem>
-              <Link to="/">Home</Link>
-            </NavItem>
-            <NavItem>
-              <Link to="/settings">Settings</Link>
-            </NavItem>
-          </NavItemsList>
-        </NavBarContainer>
-
+        <Navbar />
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
@@ -76,7 +42,6 @@ function App() {
               <h1>
                 Pomodoro Timer
               </h1>
-              
             </Main>
           </Route>
         </Switch>
@@ -86,4 +51,3 @@ function App() {
 }
 
 export default App;
-
